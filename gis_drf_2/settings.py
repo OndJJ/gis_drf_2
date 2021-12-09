c240e7c6a5b60c969e48363670d5308f0d9962de
JeonChangJun <jcjk0302@gmail.com>
2021-12-09 12:01:45 +0900

    install django Rest framework / accountapp update

--- gis_drf_2/settings.py
+++ gis_drf_2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 local_env = open(os.path.join(BASE_DIR, '.env'))
+
 env_list = dict()
 
 while True:
@@ -51,6 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accountapp',
 ]
 
 MIDDLEWARE = [
