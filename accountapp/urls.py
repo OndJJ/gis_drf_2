2ae1ce15e1b6b7785d141a60f4b8444a8e666402
JeonChangJun <jcjk0302@gmail.com>
2021-12-10 15:38:43 +0900

    DRF create Template view update

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from accountapp.views import hello_world, hello_world_template
+from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate
 
 app_name = 'accountapp'
+
+
 
 urlpatterns = [
     # UI를 보기 위한 부분
     path('hello_world_template/', hello_world_template, name='hello_world_template'),
     # 로직 처리 위한 부분
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateTemplate, name='create_template')
 ]
