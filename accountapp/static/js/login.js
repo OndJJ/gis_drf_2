f550054200bd43ce28e9261b06a6b2a73a114673
JeonChangJun <jcjk0302@gmail.com>
2021-12-13 15:31:02 +0900

    login redirect update update

--- accountapp/static/js/login.js
+++ accountapp/static/js/login.js
@@ -1,26 +1,49 @@
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    // 필요한 경우, 옵션 기본값을 설정할 수도 있습니다.
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
 function send_input() {
     axios.post('/accounts/login/', {
         username: document.getElementById('username').value,
         password: document.getElementById('password').value,
-      })
-      .then(function (response) {
-        // POST 요청이 성공했을 경우 어떤 javascript 가 구동되어야 하는지
-        console.log(response);
+    })
+        .then(function (response) {
+            // 성공했을 경우
+            console.log(response);
 
-        document.getElementById('alert_box').innerHTML
-            = "<div class='btn btn-primary rounded-pill px-5'>로그인이 성공했습니다</div>";
+            // Token 수령 후 쿠키 생성
+            setCookie('drf_token', 'Token ' + response.data['token']);
 
-        // Token 수령 후 쿠키 생성
-        document.cookie = "drf_token=Token" + response.data['token'];
+            // success_url 재연결
+            window.location.href = '/accounts/hello_world_template/';
 
-        //window.location.href= '/accounts/hello_world_template/'
+        })
+        .catch(function (error) {
+            // 실패했을 경우
+            console.log(error);
 
-      })
-      // 실패 했을 경우
-      .catch(function (error) {
-        console.log(error);
-
-        document.getElementById('alert_box').innerHTML
-            = "<div class='btn btn-danger rounded-pill px-5'>로그인이 실패했습니다</div>";
-      });
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-danger rounded-pill px-5'>로그인이 실패했습니다</div>";
+        });
 }
