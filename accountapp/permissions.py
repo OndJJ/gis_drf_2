dac8fa626961bcf0ed917300957f694544c2a476
JeonChangJun <jcjk0302@gmail.com>
2021-12-14 14:52:25 +0900

    permission update

--- accountapp/permissions.py
+++ accountapp/permissions.py
@@ -4,6 +4,6 @@ from rest_framework.permissions import BasePermission
 
 class IsOwner(BasePermission):
 
-    def has_permission(self, request, view):
-        user = User.objects.get(pk=view.kwargs['pk'])
-        return request.user == user
+    def has_object_permission(self, request, view, obj):
+        # Instance must have an attribute named `owner`.
+        return obj == request.user
